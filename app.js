a02873e9a1f656036e782e06bd16d99f8042117e
shtrvv <shatrova.liza.21@gmail.com>
2024-03-25 18:23:47 +0200

    upd app.js & server.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import contactsRouter from "./routes/contactsRouter.js";
 import authRouter from "./routes/authRoutes.js";
 import "./db.js";
 
-const app = express();
+export const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
@@ -24,8 +24,4 @@ app.use((_, res) => {
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
-});
-
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
 });
